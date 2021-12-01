d8ae13451d8a65e01f48c51496f33863c24cb737
Eduardo Ellery <eduellery@gmail.com>
2021-12-01 22:14:06 +0100

    2021 - Day 01

---
+++ 2021/kotlin/01.kt
@@ -0,0 +1,13 @@
+import java.io.File
+
+fun String.intLines(): List<Int> {
+    return File(this).readLines().map { it.toInt() }
+}
+
+fun main() {
+    val input = "01.in".intLines()
+    val part1 = input.windowed(2).filter { (a, b) -> b > a }.count()
+    val part2 = input.windowed(3).map { it.sum() }.windowed(2).filter { (a, b) -> b > a }.count()
+    println("P1: " + part1)
+    println("P2: " + part2)
+}
