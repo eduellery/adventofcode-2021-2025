bfb833e1648af9fb29e55185a50b3b4f504806f7
Eduardo Ellery <eduellery@gmail.com>
2021-12-03 18:08:31 +0100

    2021 - Day 02 (refactor)

--- 2021/kotlin/d02.kt
+++ 2021/kotlin/d02.kt
@@ -1,11 +1,11 @@
 import java.io.File
 
-fun String.intLines(): List<Pair<String, Int>> {
+fun String.pairLines(): List<Pair<String, Int>> {
     return File(this).readLines().map { Pair(it.substringBefore(" "), it.substringAfter(" ").toInt()) }
 }
 
 fun main() {
-    val input = "d02.in".intLines()
+    val input = "d02.in".pairLines()
     var depth1 = 0
     var depth2 = 0
     var horizontal = 0
