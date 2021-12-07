beb8eda69ddc9ddcd513704ccd33de8207ef05f9
Eduardo Ellery <eduellery@gmail.com>
2021-12-07 17:15:08 +0100

    2021 - Day 02 (refactor)

--- 2021/kotlin/d02.kt
+++ 2021/kotlin/d02.kt
@@ -1,32 +1,27 @@
 import java.io.File
 
-fun String.pairLines(): List<Pair<String, Int>> {
-    return File(this).readLines().map { Pair(it.substringBefore(" "), it.substringAfter(" ").toInt()) }
-}
+fun String.pairs(): List<Pair<String, Int>> =
+    File(this).readLines().map { Pair(it.substringBefore(" "), it.substringAfter(" ").toInt()) }
 
 fun main() {
-    val input = "d02.in".pairLines()
-    var depth1 = 0
-    var depth2 = 0
-    var horizontal = 0
-    var aim = 0
-    input
-            .forEach {
-                when (it.first) {
-                    "forward" -> {
-                        horizontal += it.second;
-                        depth2 += aim * it.second;
-                    }
-                    "down" -> {
-                        depth1 += it.second;
-                        aim += it.second;
-                    }
-                    "up" -> {
-                        depth1 -= it.second;
-                        aim -= it.second;
-                    }
-                }
+    val input = "d02.in".pairs()
+    var (depth1, depth2, horizontal, aim) = arrayOf(0, 0, 0, 0)
+    input.forEach {
+        when (it.first) {
+            "forward" -> {
+                horizontal += it.second
+                depth2 += aim * it.second
             }
-    println("P1: " + depth1 * horizontal)
-    println("P2: " + depth2 * horizontal)
+            "down" -> {
+                depth1 += it.second
+                aim += it.second
+            }
+            "up" -> {
+                depth1 -= it.second
+                aim -= it.second
+            }
+        }
+    }
+    println("P1: ${depth1 * horizontal}")
+    println("P2: ${depth2 * horizontal}")
 }
