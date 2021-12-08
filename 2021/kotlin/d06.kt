47b33a7830c3eb82d33b5aa25e44cda6fa145193
Eduardo Ellery <eduellery@gmail.com>
2021-12-08 19:11:21 +0100

    2021 - Day 06 (refactor)

--- 2021/kotlin/d06.kt
+++ 2021/kotlin/d06.kt
@@ -2,19 +2,22 @@ import java.io.File
 
 private fun String.ints(): List<Int> = File(this).readLines().single().split(",").map { it.toInt() }
 
-private fun simulateLanternFish(ages: List<Int>, days: Int): Long {
-    var fish = MutableList<Long>(9) { index -> ages.count { it == index }.toLong() }
+private fun LongArray.rotateLeftInPlace() {
+    val leftMost = first()
+    this.copyInto(this, startIndex = 1)
+    this[this.lastIndex] = leftMost
+}
+
+private fun simulateLanternFish(fishes: LongArray, days: Int): Long {
     repeat(days) {
-        val spawns = fish.first()
-        fish = fish.drop(1).toMutableList()
-        fish[6] += spawns
-        fish.add(spawns)
+        fishes.rotateLeftInPlace()
+        fishes[6] += fishes[8]
     }
-    return fish.sum()
+    return fishes.sum()
 }
 
 fun main() {
-    val numbers = "d06.in".ints()
-    println("P1: ${simulateLanternFish(numbers, 80)}")
-    println("P2: ${simulateLanternFish(numbers, 256)}")
+    var fishes = LongArray(9).apply { "d06.in".ints().forEach { this[it] += 1L } }
+    println("P1: ${simulateLanternFish(fishes, 80)}")
+    println("P2: ${simulateLanternFish(fishes, 256 - 80)}")
 }
